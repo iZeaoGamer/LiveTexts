0f7443f20d0b921f2984f5b416b57b35eb309fa2
caspervanneck <nethercube.host@gmail.com>
2016-11-05 11:17:44 +0100

    fix i tink the eror

--- src/LiveTexts/Text.php
+++ src/LiveTexts/Text.php
@@ -64,13 +64,13 @@
     $pk->pitch = 0;
     $pk->item = Item::get(0);
     $pk->metadata = [
-				Entity::DATA_FLAGS => [Entity::DATA_TYPE_BYTE, 1 <<    Entity::DATA_FLAG_INVISIBLE],
+				//Entity::DATA_FLAGS => [Entity::DATA_TYPE_BYTE, 1 <<    Entity::DATA_FLAG_INVISIBLE],
 				Entity::DATA_NAMETAG => [Entity::DATA_TYPE_STRING, $this->text],
 				Entity::DATA_SHOW_NAMETAG => [Entity::DATA_TYPE_BYTE, 1],
 				Entity::DATA_NO_AI => [Entity::DATA_TYPE_BYTE, 1],
 				Entity::DATA_LEAD_HOLDER => [Entity::DATA_TYPE_LONG, -1],
 				Entity::DATA_LEAD => [Entity::DATA_TYPE_BYTE, 0]
-	                        Entity::DATA_FLAGS => [Entity::DATA_FLAG_INVISIBLE, 1],
+	                        Entity::DATA_FLAG_INVISIBLE => [Entity::DATA_TYPE_BYTE, 1],
        ];
     $p->dataPacket($pk);
     parent::spawnTo($p);
