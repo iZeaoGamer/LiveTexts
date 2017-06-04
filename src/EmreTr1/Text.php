64f6c48e729c6559dff54f249a8645cede6cd568
Emre Yavuzyiğit <EmreTr1@users.noreply.github.com>
2017-06-04 15:21:50 +0300

    Add files via upload

--- src/EmreTr1/Text.php
+++ src/EmreTr1/Text.php
@@ -4,20 +4,28 @@ namespace EmreTr1;
 
 use pocketmine\entity\{Entity, Zombie};
 use pocketmine\Player;
+use pocketmine\event\entity\EntityDamageEvent;
 
 class Text extends Zombie{
+	
+	public function attack($damage, EntityDamageEvent $event){
+		$event->setCancelled(true);
+		parent::attack($damage, $event);
+	}
 	
 	public function onUpdate($tick){
 		if($this->closed) return;
 		
 		if(time() % 3 == 0){
-		 $this->setNameTag(LiveTexts::replaceText($this->getNameTag()));
+		 $this->setNameTag(LiveTexts::replacedText($this->getNameTag()));
 		}
 	}
 	
 	public function spawnTo(Player $p){
 		$this->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_INVISIBLE, true);
 		$this->setDataFlag(Entity::DATA_FLAGS, Entity::DATA_FLAG_IMMOBILE, true);
+		$this->setNameTagAlwaysVisible(true);
+		$this->setNameTagVisible(true);
 		$this->setNameTag(LiveTexts::replaceForPlayer($p, $this->getNameTag()));
 		
 		parent::spawnTo($p);
