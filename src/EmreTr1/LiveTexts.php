e8aac129b3034470054ba7e55e14fbcfe1e2fe92
iZeaoGamer <jameshayward14@icloud.com>
2017-07-25 16:52:38 +0100

    Update LiveTexts.php

--- src/EmreTr1/LiveTexts.php
+++ src/EmreTr1/LiveTexts.php
@@ -109,7 +109,7 @@ class LiveTexts extends PluginBase implements Listener{
 		$onlines=count($server->getOnlinePlayers());
 		$maxplayers=$server->getMaxPlayers();
 		$worldsc=count($server->getLevels());
-		$network=$server->getTicksPerSecond();
+		$server->getServer();
 		$variables=[
 		"{line}"=>"\n",
 		"{tps}"=>$tps,
